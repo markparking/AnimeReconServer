112f938a8c57e6278db3523fc0431ab5076d1ac5
Mark Parking <mark2908@elev.tec.dk>
2023-06-29 10:02:26 +0200

    Torsdagsændringer
    Gjort så API henter flere nodes, og sætter i cards på index

--- Data/Fetch.cs
+++ Data/Fetch.cs
@@ -1,33 +1,82 @@
 ﻿using Newtonsoft.Json.Linq;
 using RestSharp;
 using AnimeReconServer.Data;
+using Newtonsoft.Json;
 
 namespace AnimeReconServer.Data
 {
     public class Fetch
     {
-        public static API FetchData()
+        public class API
         {
-            RestClient client = new("https://api.myanimelist.net/v2/anime/10357?fields=rank,mean,alternative_titles,synopsis");
-            RestRequest req = new();
-            //req.AddHeader("Content-Type", "application/json");
+            public int id { get; set; }
+            public string title { get; set; }
+            public MainPicture main_picture { get; set; }
+            public int rank { get; set; }
+            public double mean { get; set; }
+            public Titles alternative_titles { get; set; }
+            public string synopsis { get; set; }
 
-            req.AddHeader("X-MAL-CLIENT-ID", "39f2ef8e9dca31929cc161e16ba38024");
+        }
 
-            RestResponse res = client.Execute(req);
-            // System.Diagnostics.Debug.WriteLine(res.Content);
-            // Console.WriteLine(res.Content);
-            JObject json = JObject.Parse(res.Content);
-            dynamic data = json;
-            API api = new API();
-            api.ID = data.id;
-            api.Title = data.title;
-            api.PictureURL = data.main_picture.large;
-            api.Rank = data.rank;
-            api.Mean = data.mean;
-            api.EN_Title = data.alternative_titles.en;
-            api.Synopsis = data.synopsis;
-            return api;
+        public class Titles
+        {
+            public string en { get; set; }
+            public string jp { get; set; }
+        }
+
+        public class MainPicture
+        {
+            public string medium { get; set; }
+            public string large { get; set; }
+        }
+
+        public class APIItem
+        {
+            public API node { get; set; }
+        }
+
+        public class APIResponse
+        {
+            public List<APIItem> data { get; set; }
+            public Paging paging { get; set; }
+        }
+
+        public class Paging
+        {
+            public string next { get; set; }
+        }
+
+        public class FetchApi
+        {
+            public static APIResponse Fetch()
+            {
+                RestClient client = new RestClient("https://api.myanimelist.net/v2/anime?q=one&limit=16&fields=rank,mean,alternative_titles,synopsis");
+                RestRequest req = new RestRequest();
+                req.AddHeader("X-MAL-CLIENT-ID", "39f2ef8e9dca31929cc161e16ba38024");
+                RestResponse res = client.Execute(req);
+                var json = res.Content;
+
+                // Deserialize the JSON into an APIResponse object
+                return JsonConvert.DeserializeObject<APIResponse>(json);
+            }
+
+            public static List<API> GetItems()
+            {
+                APIResponse aPIResponse = Fetch();
+                List<APIItem> items = new List<APIItem>();
+                foreach (APIItem item in aPIResponse.data)
+                {
+                    items.Add(item);
+                }
+                List<API> result = new List<API>();
+                foreach (APIItem item in items)
+                {
+                    result.Add(item.node);
+                }
+
+                return result;
+            }
         }
     }
 }
